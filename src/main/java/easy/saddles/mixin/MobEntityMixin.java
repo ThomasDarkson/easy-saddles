a352f54542a6bff2b3a7a2279ae3d29cc2de405d
Thomas Darkson <152335332+ThomasDarkson@users.noreply.github.com>
2025-04-20 17:13:28 +0300

    1.0.0

---
+++ src/main/java/easy/saddles/mixin/MobEntityMixin.java
@@ -0,0 +1,51 @@
+package easy.saddles.mixin;
+
+import net.minecraft.entity.Saddleable;
+import net.minecraft.entity.SaddledComponent;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import java.lang.reflect.Field;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MobEntity.class)
+
+public class MobEntityMixin {
+	@Inject(at = @At("HEAD"), method = "interactMob", cancellable = true)
+	protected void interactMob(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> info) {
+    	MobEntity mob = (MobEntity) (Object) this;
+		if (mob instanceof Saddleable saddleMob) {
+			boolean playerGood = player.isSneaking();
+			if (playerGood && saddleMob.isSaddled()) {
+				for (Field i : mob.getClass().getDeclaredFields()) {
+					i.setAccessible(true);
+					try {
+						Object o = i.get(mob);
+						if (o instanceof SaddledComponent component) 
+						{
+							if (mob.getWorld() instanceof ServerWorld serverWorld) {
+								mob.dropStack(serverWorld, new ItemStack(Items.SADDLE));
+								component.setSaddled(false);
+								player.swingHand(hand, (player instanceof ServerPlayerEntity));
+							}
+						}
+					} 
+					catch (IllegalArgumentException e) {
+						e.printStackTrace();
+					} catch (IllegalAccessException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+}
